57f6bc230c9d77545d958f4a4519726cd2cd44dc
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-07-13 22:55:38 -0300

    feat: cria rotas e versao inicial do roteamento do login para a tela inicial

--- seu/src/App.js
+++ seu/src/App.js
@@ -1,14 +1,15 @@
-import Cabecalho from "./componentes/compartilhado/Cabecalho";
-import SecaoExplorarEventos from "./componentes/compartilhado/SecaoExplorarEventos/SecaoExplorarEventos";
+import { Routes, Route } from 'react-router-dom';
 import Login from "./componentes/Login/Login";
+import PaginaInicial from './pages/PaginaInicial';
+import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <Cabecalho></Cabecalho>
-      <SecaoExplorarEventos></SecaoExplorarEventos>
-    </div>
-  );
+  return React.createElement(
+    Routes,
+    null,
+    React.createElement(Route, { path: "/", element: React.createElement(Login) }),
+    React.createElement(Route, { path: "/paginaInicial", element: React.createElement(PaginaInicial) })
+  )
 }
 
 export default App;
---
+++ seu/src/pages/PaginaInicial.js
@@ -0,0 +1,13 @@
+import Cabecalho from "../componentes/compartilhado/Cabecalho";
+import SecaoExplorarEventos from "../componentes/compartilhado/SecaoExplorarEventos/SecaoExplorarEventos";
+
+const PaginaInicial = () =>{
+    return(
+        <>
+            <Cabecalho />
+            <SecaoExplorarEventos />
+        </>
+    );
+}
+
+export default PaginaInicial
