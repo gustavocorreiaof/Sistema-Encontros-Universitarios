896357c9e17ecffddb3a0641d90a411781fafaca
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-07-14 01:10:24 -0300

    style: estiliza a imagem e titulos da tela de login

--- seu/src/componentes/CamposDeTexto/CPF/Index.js
+++ seu/src/componentes/CamposDeTexto/CPF/Index.js
@@ -1,4 +1,6 @@
 import '../CampoTexto.css'
+import './CPF.css'
+import Icon from 'feather-icons-react';
 
 const CPF = ({ valor, aoAlterado}) => {
 
@@ -9,12 +11,18 @@ const CPF = ({ valor, aoAlterado}) => {
     return (
         <div className="campo-texto">
             <label>CPF</label>
-            <input
-                value={valor}
-                onChange={aoDigitado}
-                required={true}
-                placeholder="Digite seu CPF"
-            />
+            <div className="campo-texto__container">
+                <span className="campo-texto_icone_esquerda">
+                    <Icon icon='user' />
+                </span>
+
+                <input className='campo-texto-cpf'
+                    value={valor}
+                    onChange={aoDigitado}
+                    required={true}
+                    placeholder="Digite seu CPF"
+                />
+            </div>
         </div>
     )
 }
--- seu/src/componentes/CamposDeTexto/Senha/Senha.js
+++ seu/src/componentes/CamposDeTexto/Senha/Senha.js
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import { FiEye, FiEyeOff } from 'react-icons/fi'
 import './Senha.css'
 import '../CampoTexto.css'
+import { useState } from 'react'
+import { FiEye, FiEyeOff } from 'react-icons/fi'
+import Icon from 'feather-icons-react';
 
 const Senha = ({ valor, aoAlterado }) => {
     const [visivel, setVisivel] = useState(false)
@@ -14,12 +15,16 @@ const Senha = ({ valor, aoAlterado }) => {
         <div className="campo-texto">
             <label>Senha</label>
             <div className="campo-texto__container">
+                <span className="campo-texto_icone_esquerda">
+                    <Icon icon='lock' />
+                </span>
+                
                 <input
                     type={visivel ? "text" : "password"}
                     value={valor}
                     onChange={e => aoAlterado(e.target.value)}
                     required={true}
-                    placeholder="Digite sua Senha..."
+                    placeholder="Digite sua Senha"
                 />
                 <span className="campo-texto__icone" onClick={alternarVisibilidade}>
                     {visivel ? <FiEyeOff size={24} /> : <FiEye size={24} />}
