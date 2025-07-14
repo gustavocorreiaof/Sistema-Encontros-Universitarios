c4d4fcda91645c37424c7de3bbf5cca57282380a
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-07-13 22:56:38 -0300

    feat: efetua a request para a api antiga e ja faz um tratamento de erros inicial

--- seu/src/componentes/Login/Login.js
+++ seu/src/componentes/Login/Login.js
@@ -3,17 +3,36 @@ import Botao from '../Botao'
 import CPF from '../CamposDeTexto/CPF/Index'
 import './Login.css'
 import Senha from '../CamposDeTexto/Senha/Senha'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
 
     const [cpf, setCpf] = useState('')
     const [senha, setSenha] = useState('')
+    const [erro, setErro] = useState('');
+    const navigate = useNavigate();
 
-    const aoSalvar = (evento) => {
-        evento.preventDefault()
-        setCpf('')
-        setSenha('')
-    }
+    const efetuarLogin = async (evento) => {
+        try {
+            evento.preventDefault();
+
+            const cpfSemCaracteres = cpf.replace(/[^0-9]/g, '');
+
+            const response = await axios.get('http://localhost:8080/alunos/' + cpfSemCaracteres);
+
+            if(response.data === false){
+                console.log('teste')
+            }
+            else if (response.status === 200) {
+                navigate('/paginaInicial');
+            }
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     const formatarCPF = (valor) => {
         valor = valor.replace(/\D/g, '')
@@ -34,7 +53,7 @@ const Login = () => {
 
     return (
         <section className="formulario">
-            <form onSubmit={aoSalvar}>                
+            <form onSubmit={efetuarLogin}>                
                 <CPF 
                     obrigatorio={true}
                     label="CPF"
